0439e1ca2f0a12461ebd0383fd27cdba9783c1dc
luckyjourney <494590923@qq.com>
2016-01-23 16:01:03 +0800

    modify gulpfile.js and package.json name must full with no space

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 ﻿echo nodejs环境下，先全局安装npm install gulp -g -d
 echo 安装以下gulp插件，之后本地安装gulp：npm install gulp
 echo npm install gulp gulp-clean gulp-less gulp-less-sourcemap less-plugin-clean-css gulp-minify-css gulp-concat gulp-uglify gulp-livereload jshint gulp-jshint gulp-notify gulp-rename gulp-imagemin gulp-content-includer gulp-connect --save-dev
-npm install gulp gulp-clean gulp-less gulp-less-sourcemap less-plugin-clean-css gulp-minify-css gulp-concat gulp-uglify gulp-livereload jshint gulp-jshint gulp-notify gulp-rename gulp-imagemin gulp-rev gulp-rev-collector gulp-content-includer gulp-connect --save-dev
+npm install gulp-clean gulp-less gulp-less-sourcemap less-plugin-clean-css gulp-minify-css gulp-concat gulp-uglify gulp-livereload jshint gulp-jshint gulp-notify gulp-rename gulp-imagemin gulp-rev gulp-rev-collector gulp-content-includer gulp-connect --save-dev
 echo npm install gulp
